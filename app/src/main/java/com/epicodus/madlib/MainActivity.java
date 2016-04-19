88f632938ba9510425e7be0dea48dac365a6f3a6
chris houghton and kevin mattison <mattisonii@yahoo.com>
2016-04-19 10:45:16 -0700

    Create button functionality (sort of)

--- app/src/main/java/com/epicodus/madlib/MainActivity.java
+++ app/src/main/java/com/epicodus/madlib/MainActivity.java
@@ -2,12 +2,24 @@ package com.epicodus.madlib;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private Button mSubmitButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mSubmitButton = (Button) findViewById(R.id.submitButton);
+            mSubmitButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Toast.makeText(MainActivity.this, "Hi, this works", Toast.LENGTH_LONG).show();
+                }
+            });
     }
 }
