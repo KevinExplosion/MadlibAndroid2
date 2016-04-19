34fd2788a62f0de5ab3b3815ebac36630fdf94af
chris houghton and kevin mattison <cehoughton71@gmail.com>
2016-04-19 11:31:54 -0700

    Gather user input for madlib.

--- app/src/main/java/com/epicodus/madlib/MainActivity.java
+++ app/src/main/java/com/epicodus/madlib/MainActivity.java
@@ -1,12 +1,27 @@
 package com.epicodus.madlib;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    public static final String TAG = MainActivity.class.getSimpleName();
+    private EditText mAdj1;
+    private EditText mAdj2;
+    private EditText mAdj3;
+    private EditText mAdverb;
+    private EditText mNoun1;
+    private EditText mNoun2;
+    private EditText mNoun3;
+    private EditText mNoun4;
+    private EditText mNoun5;
+    private EditText mNoun6;
+    private EditText mPluralNoun;
     private Button mSubmitButton;
 
     @Override
@@ -14,11 +29,37 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mAdj1 = (EditText) findViewById(R.id.adj1);
+        mAdj2 = (EditText) findViewById(R.id.adj2);
+        mAdj3 = (EditText) findViewById(R.id.adj3);
+        mAdverb = (EditText) findViewById(R.id.adverb);
+        mNoun1 = (EditText) findViewById(R.id.noun1);
+        mNoun2 = (EditText) findViewById(R.id.noun2);
+        mNoun3 = (EditText) findViewById(R.id.noun3);
+        mNoun4 = (EditText) findViewById(R.id.noun4);
+        mNoun5 = (EditText) findViewById(R.id.noun5);
+        mNoun6 = (EditText) findViewById(R.id.noun6);
+        mPluralNoun = (EditText) findViewById(R.id.pluralNoun);
+
         mSubmitButton = (Button) findViewById(R.id.submitButton);
             mSubmitButton.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                    Toast.makeText(MainActivity.this, "Hi, this works", Toast.LENGTH_LONG).show();
+                    String adj1 = mAdj1.getText().toString();
+                    String adj2 = mAdj2.getText().toString();
+                    String adj3 = mAdj3.getText().toString();
+                    String adverb = mAdverb.getText().toString();
+                    String noun1 = mNoun1.getText().toString();
+                    String noun2 = mNoun2.getText().toString();
+                    String noun3 = mNoun3.getText().toString();
+                    String noun4 = mNoun4.getText().toString();
+                    String noun5 = mNoun5.getText().toString();
+                    String noun6 = mNoun6.getText().toString();
+                    String pluralNoun = mPluralNoun.getText().toString();
+                    Log.d(TAG, adj1);
+                    Log.d(TAG, adj2);
+                    Intent intent = new Intent(MainActivity.this, MadlibActivity.class);
+                    startActivity(intent);
                 }
             });
     }
